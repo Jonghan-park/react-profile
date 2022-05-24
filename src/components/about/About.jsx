a8322ecc25c3735ae6a8ad83db8f9d832c1d67ff
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-24 09:27:40 +0900

    Update about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.png'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me-about" />
+          </div>
+        </div>
+
+        <div className="about__content">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
