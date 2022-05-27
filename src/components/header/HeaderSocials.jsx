cd27e237f03352e1baefa7516d8bf4c453bd9998
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-27 16:41:10 +0900

    Update portfolio

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
       <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-      <a href="https://github.com" target="_blank"><BsGithub /></a>
+      <a href="https://github.com/Jonghan-park" target="_blank"><BsGithub /></a>
     </div>
   )
 }
