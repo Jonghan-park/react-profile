d8f0742f37b3c76ac874786ed2fbf306ecc7686a
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-29 10:13:56 +0900

    Update images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './portfolio.css'
-// import IMG1 from '../../assets/portfolio1.png'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
 
 const Portfolio = () => {
   return (
@@ -11,7 +13,7 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-
+            <img src={IMG1} alt="image1" />
           </div>
           <h3>This is a portfolio item title</h3>
           <a href="https://github.com/Jonghan-park" className='btn' target="_blank">Github</a>
