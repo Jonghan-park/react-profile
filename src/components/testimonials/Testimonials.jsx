4e983d2710218d05d2e400abebe1376f1c926f81
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-01 21:54:47 +0900

    Update testimonial pic

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/profile1.png'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="profile picture" />
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
