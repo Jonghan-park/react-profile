7573b6ab3d9d324492ece5d6518938040accdd85
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-02 02:27:30 +0900

    Update contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,21 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+
+          </article>
+          <div>
+            <form action=""></form>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
