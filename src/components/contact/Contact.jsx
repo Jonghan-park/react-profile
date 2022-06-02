4297899ebc1303f6a89acbcc435cab35db3af0fd
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-02 18:17:04 +0900

    Update emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8d2ho99', 'template_voop4u7', form.current, 'iTmvadtJOBsqWZ72S')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -24,7 +41,7 @@ const Contact = () => {
             <a href="https://api.whatsapp.com/send?phone=18257121504" target="_blank">Send a message</a>
           </article>
           </div>
-            <form action="">
+            <form ref={form} onSubmit={sendEmail}>
               <input type="text" name='name' placeholder='Your Full Name' required />
               <input type="email" name='email' placeholder='Your Email' required />
               <textarea name="message" rows="7" placeholder='Your Message' required />
