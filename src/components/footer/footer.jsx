95cf89d350393f3493e24943385c1c338eca0762
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-04 12:03:32 +0900

    Update footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,23 @@ import './footer.css'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Jonghan</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://"></a>
+      </div>
+    </footer>
   )
 }
 
